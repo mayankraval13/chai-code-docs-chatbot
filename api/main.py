6f6b0f22c63e744a6dc641babed78edf661efb8b
mayankraval13 <iammayankraval@gmail.com>
2025-06-13 19:00:21 +0530

    updated main.py

--- api/main.py
+++ api/main.py
@@ -3,12 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from rag_core import get_answer
 
 app = FastAPI()
-
-# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -16,5 +13,6 @@ app.add_middleware(
 @app.post("/chat")
 async def chat(request: Request):
     data = await request.json()
-    query = data.get("query", "")
-    return get_answer(query)
+    return get_answer(data.get("query", ""))
+
+handler = app  # This is essential
